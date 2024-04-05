579618307d37352c5fc397e920af0bb8b97d8572
박 영진 <bthsyj@gmail.com>
2024-04-05 20:29:11 +0900

    fix: 엔티티 수정, 패키지 정리

--- src/main/java/com/DogFoot/adpotAnimal/products/dto/ProductDto.java
+++ src/main/java/com/DogFoot/adpotAnimal/products/dto/ProductDto.java
@@ -1,6 +1,6 @@
-package com.DogFoot.adpotAnimal.dto;
+package com.DogFoot.adpotAnimal.products.dto;
 
-import com.DogFoot.adpotAnimal.entity.Product;
+import com.DogFoot.adpotAnimal.products.entity.Product;
 import lombok.*;
 
 @Data
@@ -9,19 +9,19 @@ import lombok.*;
 public class ProductDto {
     private Long id;
     private Integer category_id;
-    private int price;
+    private int product_price;
     private String product_name;
     private int product_stock;
-    private int like;
+    private int product_like;
 
     public static Product toEntity(ProductDto productDto) {
         Product product = new Product();
         product.setId(productDto.getId());
         product.setCategory_id(productDto.getCategory_id());
-        product.setPrice(productDto.getPrice());
+        product.setProduct_price(productDto.getProduct_price());
         product.setProduct_name(productDto.getProduct_name());
         product.setProduct_stock(productDto.getProduct_stock());
-        product.setLike(productDto.getLike());
+        product.setProduct_like(productDto.getProduct_like());
         return product;
     }
 
@@ -29,10 +29,10 @@ public class ProductDto {
         ProductDto productDto = new ProductDto();
         productDto.setId(product.getId());
         productDto.setCategory_id(product.getCategory_id());
-        productDto.setPrice(product.getPrice());
+        productDto.setProduct_price(product.getProduct_price());
         productDto.setProduct_name(product.getProduct_name());
         productDto.setProduct_stock(product.getProduct_stock());
-        productDto.setLike(product.getLike());
+        productDto.setProduct_like(product.getProduct_like());
         return productDto;
     }
 }
