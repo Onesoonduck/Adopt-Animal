5b8b61c80ff1e610ece98612e9a6ef73b12f1c08
jiwon2030 <jiwon5568@gmail.com>
2024-04-17 17:12:00 +0900

    Fix: dto 추가에 따른 메소드 수정

--- src/main/java/com/DogFoot/adpotAnimal/categories/repository/CategoryRepository.java
+++ src/main/java/com/DogFoot/adpotAnimal/categories/repository/CategoryRepository.java
@@ -1,12 +1,12 @@
 package com.DogFoot.adpotAnimal.categories.repository;
 
-import com.DogFoot.adpotAnimal.categories.entity.Categories;
+import com.DogFoot.adpotAnimal.categories.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CategoriesRepository extends JpaRepository<Categories, Long> {
-    Optional<Categories> findById(Long categoryId);
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findById(Long categoryId);
 }
