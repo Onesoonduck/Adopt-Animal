9630b4ccaf125a00310330fac0ae33b77c45e88b
박 영진 <bthsyj@gmail.com>
2024-04-19 03:08:58 +0900

    feat: productdetail image 추가

--- src/main/resources/static/js/productdetail.js
+++ src/main/resources/static/js/productdetail.js
@@ -41,7 +41,7 @@ async function displayProductDetail(productId) {
         const html = `
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src=${productDto.productImg} alt="..." /></div>
                     <div class="col-md-6">
                         <h1 class="display-5 fw-bolder" id="productName">${productDto.productName}</h1>
                         <div class="fs-5 mb-5" id="productPrice">
@@ -57,7 +57,7 @@ async function displayProductDetail(productId) {
                             </a>
                             <button class="btn btn-outline-dark flex-shrink-0" type="button">
                                 <i class="bi-cart-fill me-1"></i>
-                                장바구니에 추가-
+                                장바구니에 추가
                             </button>
                         </div>
                     </div>
