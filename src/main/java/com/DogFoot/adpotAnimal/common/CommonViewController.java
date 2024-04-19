3b8d0a6d6b1760fbdb61d7503813b076ddfaa67c
h3136514 <3136514@naver.com>
2024-04-20 00:54:35 +0900

    feat: 초기주소 설정

--- src/main/java/com/DogFoot/adpotAnimal/common/CommonViewController.java
+++ src/main/java/com/DogFoot/adpotAnimal/common/CommonViewController.java
@@ -2,9 +2,13 @@ package com.DogFoot.adpotAnimal.common;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class CommonViewController {
-
+    @GetMapping("/")
+    public String mainview() {
+        return "redirect:/main.html";
+    }
 }
